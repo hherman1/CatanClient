a8febb9a925586aacfa7f6b2127e0b0208c78fa3
Hunter <hherman1@macalester.edu>
2016-02-09 15:47:43 -0600

    wireframe

--- Canvas.js
+++ Canvas.js
@@ -1,46 +1,66 @@
-/* optional object
- * Board object:
- * {camera: {location:{x:int,y:int},
- *           zoom:int},
- *  vertices: [coords]
- *  hexes: [coords]
+/*
+ * board data:
+ * {buildings: [(coords,(color,building))]
+ *  roads:[(coords,coords,color)],
+ *  tiles: [(vector,tiletype)],
  *  }
+ * Goal: become capable of rendering board objects 
+ *
+ * Next Steps:
+ * - Render Thiefs
+ * - Render trade ports
+ * - ?
  */
 
-
-function drawBoard(board) {
+function drawBoard(board,ctx) {
 
 }
 
-function drawRect(coords,diameter,ctx) {
-        ctx.fillRect(coords.x,coords.y,diameter,diameter)
+
+//takes the start and end points of the road and a color and draws it on the ctx
+function drawRoad(verta,vertb,color,ctd) {
+
 }
-function hexPath(hexCoords,diameter,ctx) {
+
+//takes the vertex coordinate, color, and building type and draws it on the ctx
+function drawBuilding(vert,color,building,ctx) {
+
+}
+
+// takes the hexcoord and tiletype (includes number) and draws it on the ctx
+function drawTile(hexcoord,tiletype,ctx) {
+
+}
+
+function drawRect(coords,side,ctx) {
+        ctx.fillRect(coords.x,coords.y,side,side)
+}
+function hexPath(hexCoords,side,ctx) {
         ctx.beginPath()
         var verts = vertices(hexCoords);
-        var start = center(vertexToCanvas(verts[0],diameter),ctx.canvas)
+        var start = center(vertexToCanvas(verts[0],side),ctx.canvas)
         ctx.moveTo(start.x,start.y)
         var mappingFunction = function(coord) {
-               var point = center(vertexToCanvas(coord,diameter),ctx.canvas);
+               var point = center(vertexToCanvas(coord,side),ctx.canvas);
                ctx.lineTo(point.x,point.y);
         }
         verts.map(mappingFunction)
         ctx.closePath();
 
 }
-function makeHex(hexCoords,diameter,ctx) {
-        hexPath(hexCoords,diameter,ctx)
+function makeHex(hexCoords,side,ctx) {
+        hexPath(hexCoords,side,ctx)
         ctx.fill();
 }
-function drawHexPoints(hexCoords,diameter,ctx) {
+function drawHexPoints(hexCoords,side,ctx) {
     var mappingFunction = function(coord) {
-        drawVertex(coord,diameter,ctx);
+        drawVertex(coord,side,ctx);
     }
     var coordsList = vertices(hexCoords);
     coordsList.map(mappingFunction)
 }
-function drawVertex(vertexCoords,diameter,ctx) {
-        coords = center(vertexToCanvas(vertexCoords,diameter),ctx.canvas);
+function drawVertex(vertexCoords,side,ctx) {
+        coords = center(vertexToCanvas(vertexCoords,side),ctx.canvas);
 
         ctx.fillRect(coords.x,coords.y,10,10)
 }
@@ -49,11 +69,11 @@ function center(coords,canvas) {
         return add(makeVector(canvas.width/2, canvas.height/2),coords)
 }
 
-function hexToCanvas(hexcoords,diameter) {
-    return add(makeVector(-diameter/2,-diameter/2),piecewiseTimes(makeVector(diameter,-diameter),fromHex(hexcoords)))
+function hexToCanvas(hexcoords,side) {
+    return add(makeVector(-side/2,-side/2),piecewiseTimes(makeVector(side,-side),fromHex(hexcoords)))
 }
-function vertexToCanvas(vcoords,diameter) {
-    return add(makeVector(-diameter,-diameter),piecewiseTimes(makeVector(diameter,-diameter),fromVertex(vcoords)))
+function vertexToCanvas(vcoords,side) {
+    return add(makeVector(-side,-side),piecewiseTimes(makeVector(side,-side),fromVertex(vcoords)))
 
 }
 

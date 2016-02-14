a7ffcb8f2be811458486e5fb0c118abbfc0d7644
Stephanie Duong <sduong@macalester.edu>
2016-02-14 14:40:12 -0600

    organized and created a folder for graphics for the board

--- Canvas.js
+++ Canvas.js
@@ -73,7 +73,7 @@ function drawToken(res,hcpair, token, ctx){
 
 	} else{
     var rob = new Image();
-    rob.src = 'robber.svg';
+    rob.src = 'graphics/robber.svg';
     ctx.drawImage(rob, xctx-15, yctx-(hcpair[2]*.75),hcpair[2],hcpair[2]*1.25);
 
 	}
@@ -83,12 +83,12 @@ function drawToken(res,hcpair, token, ctx){
 function getResImg(res){
   //still needs work...i will have to spend some time resizing these photos somehow
   var resources = {};
-  resources.lumber = 'forest.svg'; //'http://upload.wikimedia.org/wikipedia/commons/5/57/Pine_forest_in_Estonia.jpg'; //labeled for noncommercial reuse
-  resources.grain = 'field.svg'; //'http://s0.geograph.org.uk/geophotos/01/95/58/1955803_c2ba5c1a.jpg';//labeled for noncommercial reuse
-  resources.wool = 'pasture.svg'; //'https://upload.wikimedia.org/wikipedia/commons/d/d3/Sheep_pasture_-_geograph.org.uk_-_462124.jpg'; //labeled for noncommercial reuse
-  resources.ore = 'mountains.svg'; //'https://c2.staticflickr.com/4/3891/15098151722_ff47b2b841_b.jpg';//labeled for noncommercial reuse
-  resources.brick = 'hills.svg'; //'https://c2.staticflickr.com/6/5325/7097453311_4108c089f3_b.jpg';//labeled for noncommercial reuse
-  resources.nothing = 'desert.svg'; //"https://upload.wikimedia.org/wikipedia/commons/b/bd/Morocco_Africa_Flickr_Rosino_December_2005_84514010.jpg"; //labeled for noncommercial reuse
+  resources.lumber = 'graphics/forest.svg'; //'http://upload.wikimedia.org/wikipedia/commons/5/57/Pine_forest_in_Estonia.jpg'; //labeled for noncommercial reuse
+  resources.grain = 'graphics/field.svg'; //'http://s0.geograph.org.uk/geophotos/01/95/58/1955803_c2ba5c1a.jpg';//labeled for noncommercial reuse
+  resources.wool = 'graphics/pasture.svg'; //'https://upload.wikimedia.org/wikipedia/commons/d/d3/Sheep_pasture_-_geograph.org.uk_-_462124.jpg'; //labeled for noncommercial reuse
+  resources.ore = 'graphics/mountains.svg'; //'https://c2.staticflickr.com/4/3891/15098151722_ff47b2b841_b.jpg';//labeled for noncommercial reuse
+  resources.brick = 'graphics/hills.svg'; //'https://c2.staticflickr.com/6/5325/7097453311_4108c089f3_b.jpg';//labeled for noncommercial reuse
+  resources.nothing = 'graphics/desert.svg'; //"https://upload.wikimedia.org/wikipedia/commons/b/bd/Morocco_Africa_Flickr_Rosino_December_2005_84514010.jpg"; //labeled for noncommercial reuse
   return resources[res];
 
 }

751f134f6d4492a60117241b8b22574e403ce48f
carstenhaas <chaas1@macalester.edu>
2016-02-23 19:21:18 -0600

    Updated settler class to player

---
+++ scripts/helper/Player.js
@@ -0,0 +1,36 @@
+//Players are assigned an ID number from 1-4. Vertices with no constructions are assigned to
+//team 0. The ID is used for the purposes of identification
+//and automatic color selection. All other values are initialized to zero.
+
+function player(playerID){
+  colList = ["red", "orange", "blue", "white"];  //Player colors will be consistently assigned
+                                                 //in accordance with their ID
+  //Player owned constructions
+  var settlementCount = 0;
+  var roadCount = 0;
+  var cityCount = 0; 
+  //Player owned resources
+  var woodCount = 0;
+  var wheatCount = 0;
+  var oreCount = 0;
+  var brickCount = 0;
+  var sheepCount = 0;
+  //Color assigned
+  var playerColor = colList[playerID-1];
+  //Player victory points
+  var vicPoints = 0;
+  var playerInfo = {
+    playerID:playerID,
+    settlement: settlementCount,
+    road: roadCount,
+    city: cityCount,
+    woodCount: woodCount,
+    wheatCount: wheatCount,
+    oreCount: oreCount,
+    brickCount: brickCount,
+    sheepCount: sheepCount,
+    playerColor: playerColor,
+    vicPoints:vicPoints
+  }
+  return playerInfo;
+}

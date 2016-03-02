91d833c5901066235c2a76b256b7214111d4c007
Hunter Herman <hherman1@macalester.edu>
2016-03-02 16:25:22 -0600

    Refactored

--- scripts/helper/Settlement.js
+++ scripts/helper/Settlement.js
@@ -6,24 +6,25 @@ function drawBuilding(vert,playerColor,side,ctx){
   img.src = getBuildingImg(vert.settled, playerColor); //set source path
   ctx.drawImage(img, vert.x, vert.y);
 }
+
+Settlements = []
+Settlements[Colors.red]     = 'graphics/reds.svg';
+Settlements[Colors.orange]  = 'graphics/oranges.svg';
+Settlements[Colors.blue]    = 'graphics/blues.svg';
+Settlements[Colors.white]   = 'graphics/whites.svg';
+
+Cities = []
+Cities[Colors.red]      = 'graphics/redc.svg';
+Cities[Colors.orange]   = 'graphics/orangec.svg';
+Cities[Colors.blue]     = 'graphics/bluec.svg';
+Cities[Colors.white]    = 'graphics/whitec.svg';
+
 function getBuildingImg(settletype, playerColor){
   //still needs work...i will have to spend some time resizing these photos somehow
-  var settlements = {
-    0 : 'graphics/reds.svg',
-    1 : 'graphics/oranges.svg',
-    2: 'graphics/blues.svg',
-    3: 'graphics/whites.svg'
-  };
-  var cities = {
-    0 : 'graphics/redc.svg',
-    1 : 'graphics/orangec.svg',
-    2 : 'graphics/bluec.svg',
-    3 : 'graphics/whitec.svg'
-  };
   if (settletype == 1){
-    return settlements[playerColor];
+    return Settlements[playerColor];
   }
   else{
-    return cities[playerColor];
+    return Cities[playerColor];
   }
 }

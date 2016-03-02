3687724ad4d9916c5d5d5dfe19ceb6c5e4fc15fb
Hunter Herman <hherman1@macalester.edu>
2016-03-02 14:05:48 -0600

    Refactoring

--- scripts/helper/Game.js
+++ scripts/helper/Game.js
@@ -1,84 +1,3 @@
-
-function Reference(data) {
-        return {data:data}
-}
-
-function newServer() {
-        return {
-                gamestate: null
-                ,getState:function() {
-                        return this.gamestate
-                }
-                ,newGame: function(width) {
-                    this.gamestate = {
-                        board: buildRegularHexFramework(width),
-                        phase: null,
-                        players: []
-                    }
-                }
-                ,addPlayer: function(player) {
-                        this.gamestate.players.push(player);
-                }
-        }
-}
-
-
-function Buffer() {
-    this.mouse = newMouseBuffer();
-    this.ui = {};
-}
-
-
-Game = function(context) {
-        this.ctx;
-        this.mouse = newMouse();
-        this.buffer = new Buffer();
-        this.graphics = new Graphics();
-        this.server = newServer();
-        this.gamestate;
-        this.hitboxes;  
-        this.ui;
-}
-
-initGame = function(game,ctx) {
-        var canvas = ctx.canvas;
-        game.ctx = ctx;
-        game.ui = new UI(canvas); //None?
-        game.graphics.transform.translation = center(makeVector(canvas.width,canvas.height));
-
-        //the below code may be better suited elsewhere
-
-        initMouseBuffer(canvas,game.buffer.mouse);
-        document.addEventListener("mouseup",mouseEventSaver(game.buffer.mouse.mouseups)) //Pay attention to mouse releases from anywhere in the document
-        game.server.newGame(5);
-        game.gamestate = game.server.getState();
-        game.hitboxes = 
-                genHitboxes(game.gamestate.board.map(function(tile) {return tile.coordinates})
-                           ,[]
-                           ,game.gamestate.board.map(function(tile) {return tile.coordinates})
-                           ,50);
-}
-
-Graphics = function(){
-        this.animations = new Reference([])
-        this.transform = {
-               translation: makeVector(0,0)
-              ,scale: 1
-        }
-}
-
-function UI(canvas) {
-       this.build = 0
-       this.loading = 1
-       this.mode = this.build
-}
-
-
-function runGame(game,frameDuration) {
-        window.setInterval(gameStep,frameDuration,game);
-}
-
-
 function last(list) {
         return list[list.length - 1]
 }
@@ -101,6 +20,87 @@ function newScale(delta,scale) {
         }
 }
 
+Reference = function(data) {
+        return {data:data}
+}
+
+Board = function() {
+
+}
+
+GameState = function() {
+        this.board = new Board();
+        this.phase;
+        this.players;
+}
+Graphics = function(){
+        this.animations = new Reference([])
+        this.transform = {
+               translation: makeVector(0,0)
+              ,scale: 1
+        }
+}
+
+UI = function(canvas) {
+       this.build = 0
+       this.loading = 1
+       this.mode = this.build
+}
+
+Server = function() {
+        this.gamestate = new GameState();
+        this.getState = function() {
+                return this.gamestate;
+        }
+        this.newGame = function(width) {
+            this.gamestate.board = buildRegularHexFramework(width);
+            this.gamestate.players = []
+        }
+        this.addPlayer = function(player) {
+                this.gamestate.players.push(player);
+        }
+}
+
+Buffer = function() {
+    this.mouse = newMouseBuffer();
+    this.ui = {};
+}
+
+
+Game = function(context) {
+        this.ctx;
+        this.mouse = newMouse();
+        this.buffer = new Buffer();
+        this.graphics = new Graphics();
+        this.server = new Server();
+        this.gamestate;
+        this.hitboxes;  
+        this.ui
+}
+
+initGame = function(game,ctx) {
+        var canvas = ctx.canvas;
+        game.ctx = ctx;
+        game.ui = new UI(canvas); //None?
+        game.graphics.transform.translation = center(makeVector(canvas.width,canvas.height));
+
+        //the below code may be better suited elsewhere
+
+        initMouseBuffer(canvas,game.buffer.mouse);
+        document.addEventListener("mouseup",mouseEventSaver(game.buffer.mouse.mouseups)) //Pay attention to mouse releases from anywhere in the document
+        game.server.newGame(5);
+        game.gamestate = game.server.getState();
+        game.hitboxes = 
+                genHitboxes(game.gamestate.board.map(function(tile) {return tile.coordinates})
+                           ,[]
+                           ,game.gamestate.board.map(function(tile) {return tile.coordinates})
+                           ,50);
+}
+
+function runGame(game,frameDuration) {
+        window.setInterval(gameStep,frameDuration,game);
+}
+
 function gameStep(game) {
         var hitlist = transformHitlist(game.hitboxes,game.graphics.transform);
         mouse = processBuffer(game.mouse,game.buffer.mouse);
@@ -115,8 +115,6 @@ function gameStep(game) {
         if(game.mouse.scroll.y != 0) {
                 game.graphics.transform.scale = newScale(game.mouse.scroll.y,game.graphics.transform.scale);
         }
-
-        //console.log(mouse.pos);
 
         redraw(game.gamestate.board,game.mouse,game.graphics.transform,game.graphics.animations,game.ctx);
         flushMouseEvents(game.buffer.mouse);

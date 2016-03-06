d554e8d5b6ecf78c6e0f167e705523bde6b27fd1
Stephanie Duong <sduong@macalester.edu>
2016-03-06 10:23:05 -0600

    I GOT REQUIRE.JS TO WORK YAYAYAY :)

--- www/apps/main.js
+++ www/apps/main.js
@@ -1,14 +1,40 @@
+define(function (require) {
+    // Load any app-specific modules
+    // with a relative require call,
+    // like:
+    //var messages = require('./messages');
 
-function test() {
-        return "lol"
-}
-function main() {
-        canvas = document.getElementById('board');
-        if(canvas.getContext) {
-                ctx = canvas.getContext('2d');
-                drawTitle(ctx);
-                initGame(ctx);
-        } else {
-            console.log("browser unsupported")
-        }
-}
+    var game = require('./Game');
+    var gameM = require('./GameMethods');
+    var boardstate = require('./BoardState');
+    var canvas = require('./Canvas');
+    var dice = require('./Dice');
+    var animation = require('./Animation');
+    var grid = require('./Grid');
+    var hitbox = require('./Hitbox');
+    var mouse = require('./Mouse');
+    var player = require('./Player');
+    var resource = require('./Resource');
+    var robber = require('./Robber');
+    var structrender = require('./StructureRenderer');
+    var action = require('./Action');
+    // Load library/vendor modules using
+    // full IDs, like:
+    //var print = require('print');
+
+    //print(messages.getHello());
+    // function test() {
+    //         return "lol"
+    // }
+    // function main() {
+    //         canvas = document.getElementById('board');
+    //         if(canvas.getContext) {
+    //                 ctx = canvas.getContext('2d');
+    //                 drawTitle(ctx);
+    //                 initGame(ctx);
+    //         } else {
+    //             console.log("browser unsupported")
+    //         }
+    // }
+
+});

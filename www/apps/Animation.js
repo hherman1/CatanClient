55ab4e47a02920528a8c82bfe12ef3126b5ef202
Hunter Herman <hherman1@macalester.edu>
2016-03-31 00:06:48 -0500

    Sync UI roll and canvas animation

--- www/apps/Animation.js
+++ www/apps/Animation.js
@@ -23,7 +23,7 @@ Animation = {
                                       self.frame = self.frame + 1;
                                       return draw(ctx,transform,self.frame,self.totalFrames);
                               }
-                              ,function(){return (this.frame >= this.totalFrames - 1)});
+                              ,function(){return (self.frame >= self.totalFrames - 1)});
                 self.type = Animation.Type.MultiFrame;
                 self.frame = 0;
                 self.totalFrames = frames;
@@ -41,6 +41,20 @@ Animation = {
                                 ,function() {
                                      return self.currentAnim >= anims.length;
                                 });
+        },
+        //Draws both animations simultaneously
+        //Draws background while not isOver
+        //whole animation isOver when main isOver
+        Auxiliary : function(main,background) {
+                var self = this;
+                Animation.Object.call(this
+                                ,function(ctx,transform) {
+                                        main.draw(ctx,transform);
+                                        if(!background.isOver()); {
+                                                background.draw(ctx,transform);
+                                        }
+                                }
+                                ,main.isOver);
         }
 
 }
@@ -60,102 +74,6 @@ ClickCircle = function(coordinate,radius,frames,style) {
                         ,frames)
 }
 
-DiceRoll = function(coordinate,target,max,min,radius,vert,frames) {
-
-        var self = this;
-
-        self.nextNum = function() {
-                return Math.round(Math.random() * (max - min) + min);
-        }
-        self.numCount = 12;
-        self.nums = Array.apply(null,Array(self.numCount)).map(self.nextNum);
-        self.nums[0] = target;
-
-        var textWidth = 28;
-        var spinFramesFraction = 7/10;
-        var angleDiff = 2 * Math.PI / self.nums.length;
-
-        self.rotationOffset = function(totalFrames) {
-                var totalRotation = 0;
-                for(var i = 0; i < totalFrames;i++) {
-                        totalRotation += self.getSpeed(i,totalFrames);
-                }
-                return (angleDiff*(totalFrames-1)/(7*2)) % (Math.PI*2) //Math.PI * 2 - totalRotation%(Math.PI * 2);
-        }
-
-        self.getSpeed = function(frame,totalFrames) {
-                var n = totalFrames - 1;
-                return (angleDiff/7) * (1 - (2*Math.abs(frame - n/2)/n)) 
-                        + (self.randomOffset/(totalFrames-1));
-        }
-        
-        self.randomOffset = Math.random() * 2 * Math.PI
-        self.rotation = self.rotationOffset(spinFramesFraction*frames) + self.randomOffset;
-        self.vertical = 0;
-
-        self.tween = function(verticalDiff) {
-                return function(ctx,transform,frame,totalFrames) {
-                        var n = totalFrames/2;
-                        var constant = 2/(n * (1 + n));
-                        if(frame < n) {
-                                self.vertical += 1/2 * verticalDiff  * constant * frame;
-                        } else {
-                                self.vertical += 1/2 * verticalDiff  * constant * (2*n - frame);
-                        }
-                        self.drawWheel(ctx);
-                }
-        }
-
-        self.drawBackground = function(ctx) {
-                resetTransform(ctx);
-                ctx.translate(coordinate.x,coordinate.y+self.vertical);
-
-                var fillRadius = radius + radius/3;
-
-                var gradient = ctx.createRadialGradient(0,0,fillRadius/2,0,0,fillRadius);
-                gradient.addColorStop(1,"#FFF0BD");
-                gradient.addColorStop(0,"#FFFFCD");
-                ctx.fillStyle = gradient;
-                ctx.lineWidth = 1;
-                ctx.strokeStyle = "black";
-
-                ctx.beginPath()
-                ctx.arc(0,0,fillRadius,0,2*Math.PI,false);
-                ctx.fill();
-                ctx.stroke();
-                ctx.closePath();
-        }
-        
-        self.drawWheel = function(ctx) {
-
-                self.drawBackground(ctx);
-                
-                resetTransform(ctx);
-                ctx.translate(coordinate.x,coordinate.y + self.vertical);
-                ctx.rotate(self.rotation);
-
-                ctx.font = "28px sans-serif";
-                ctx.fillStyle="rgba(0,0,0,1)";
-
-                ctx.beginPath();
-                self.nums.map(function(n) {
-                        ctx.fillText("" + n,-textWidth/2,-radius,textWidth);
-                        ctx.rotate(angleDiff);
-                })
-        }
-
-        self.drawSpin = function(ctx,transform,frame,totalFrames) {
-                resetTransform(ctx);
-                var angleDiff = 2 * Math.PI / self.nums.length;
-                self.rotation = self.rotation - self.getSpeed(frame,totalFrames);
-                self.drawWheel(ctx);
-        }
-        Animation.MultiPhase.call(self
-                        ,[new Animation.MultiFrame(self.tween(-vert),frames/10)
-                         ,new Animation.MultiFrame(self.drawSpin,spinFramesFraction*frames)
-                         ,new Animation.MultiFrame(self.drawWheel,frames/10)
-                         ,new Animation.MultiFrame(self.tween(vert),frames/10)]);
-}
 
 
 
--- www/apps/Dice.js
+++ www/apps/Dice.js
@@ -1,21 +1,19 @@
 //DICE
 //generate roll of 2 dice
 //created by sduong
-var rsum = 0;//Initialize global instance of rsum so it can be returned
 
 /*
 rollDice generates the "rolling" for two dice and stores the sum of the two rolls in rsum.
 */
-function rollDice(){
-  var rollHolder = document.getElementById("rollValue");
+function setDice(rollHolder,val) {
+      rollHolder.innerHTML = val;
+}
+
+function rollDice(target,time){
   //disableRollButton();
   for (var i = 30000; i >= 0; i--) {//TODO make the scroll part more "conventional" and less "MVP"
     setTimeout(function()
     {
-      var roll1 = Math.round(Math.random() * 5)+1;
-      var roll2 = Math.round(Math.random() * 5)+1;
-      rsum = roll1 + roll2;
-      rollHolder.innerHTML = rsum;
     }, 100);
   };
 	// if (rsum == 7){ //will have to implement elsewhere when we start worrying about moving the robber
@@ -23,20 +21,124 @@ function rollDice(){
 	// }
 }
 
-/*
-getRsum is a getter method that returns rsum.
-*/
-function getRsum(){
-	return rsum;
+
+function rollDiceValue(max) {
+      var roll1 = Math.round(Math.random() * Math.floor((max-1)/2))+1;
+      var roll2 = Math.round(Math.random() * Math.floor((max-1)/2))+1;
+      return roll1 + roll2;
 }
 
-/*
-Prints rsum on the bottomRightDisplay of the UI
-*/
-function rollUpdate(rsum){
-  var rollHolder = document.getElementById("rollValue");
-  rollHolder.innerHTML = rsum;
+DiceRollUI = function(target,max,frames) {
+        var rollHolder = document.getElementById("rollValue");
+        Animation.MultiFrame.call(this
+                        ,function(ctx,transform,frame,totalFrames) {
+                                setDice(rollHolder,rollDiceValue(max));
+                                if(totalFrames - frame <= 1) {
+                                        setDice(rollHolder,target);
+                                }
+                        }
+                        ,frames);
 }
+
+DiceRoll = function(coordinate,target,min,max,radius,vert,frames) {
+
+        var self = this;
+
+        self.nextNum = function() {
+                return Math.round(Math.random() * (max - min) + min);
+        }
+        self.numCount = 12;
+        self.nums = Array.apply(null,Array(self.numCount)).map(self.nextNum);
+        self.nums[0] = target;
+
+        var textWidth = 28;
+        var spinFramesFraction = 7/10;
+        var angleDiff = 2 * Math.PI / self.nums.length;
+
+        self.rotationOffset = function(totalFrames) {
+                var totalRotation = 0;
+                for(var i = 0; i < totalFrames;i++) {
+                        totalRotation += self.getSpeed(i,totalFrames);
+                }
+                return (angleDiff*(totalFrames-1)/(7*2)) % (Math.PI*2) //Math.PI * 2 - totalRotation%(Math.PI * 2);
+        }
+
+        self.getSpeed = function(frame,totalFrames) {
+                var n = totalFrames - 1;
+                return (angleDiff/7) * (1 - (2*Math.abs(frame - n/2)/n)) 
+                        + (self.randomOffset/(totalFrames-1));
+        }
+        
+        self.randomOffset = Math.random() * 2 * Math.PI
+        self.rotation = self.rotationOffset(spinFramesFraction*frames) + self.randomOffset;
+        self.vertical = 0;
+
+        self.tween = function(verticalDiff) {
+                return function(ctx,transform,frame,totalFrames) {
+                        var n = totalFrames/2;
+                        var constant = 2/(n * (1 + n));
+                        if(frame < n) {
+                                self.vertical += 1/2 * verticalDiff  * constant * frame;
+                        } else {
+                                self.vertical += 1/2 * verticalDiff  * constant * (2*n - frame);
+                        }
+                        self.drawWheel(ctx);
+                }
+        }
+
+        self.drawBackground = function(ctx) {
+                resetTransform(ctx);
+                ctx.translate(coordinate.x,coordinate.y+self.vertical);
+
+                var fillRadius = radius + radius/3;
+
+                var gradient = ctx.createRadialGradient(0,0,fillRadius/2,0,0,fillRadius);
+                gradient.addColorStop(1,"#FFF0BD");
+                gradient.addColorStop(0,"#FFFFCD");
+                ctx.fillStyle = gradient;
+                ctx.lineWidth = 1;
+                ctx.strokeStyle = "black";
+
+                ctx.beginPath()
+                ctx.arc(0,0,fillRadius,0,2*Math.PI,false);
+                ctx.fill();
+                ctx.stroke();
+                ctx.closePath();
+        }
+        
+        self.drawWheel = function(ctx) {
+
+                self.drawBackground(ctx);
+                
+                resetTransform(ctx);
+                ctx.translate(coordinate.x,coordinate.y + self.vertical);
+                ctx.rotate(self.rotation);
+
+                ctx.font = "28px sans-serif";
+                ctx.fillStyle="rgba(0,0,0,1)";
+
+                ctx.beginPath();
+                self.nums.map(function(n) {
+                        ctx.fillText("" + n,-textWidth/2,-radius,textWidth);
+                        ctx.rotate(angleDiff);
+                })
+        }
+
+        self.drawSpin = function(ctx,transform,frame,totalFrames) {
+                resetTransform(ctx);
+                var angleDiff = 2 * Math.PI / self.nums.length;
+                self.rotation = self.rotation - self.getSpeed(frame,totalFrames);
+                self.drawWheel(ctx);
+        }
+        Animation.MultiPhase.call(self
+                        ,[new Animation.MultiFrame(self.tween(-vert),frames/10)
+                         ,new Animation.Auxiliary(
+                                 new Animation.MultiFrame(self.drawSpin,spinFramesFraction*frames)
+                                ,new DiceRollUI(target,max,spinFramesFraction*frames))
+                         ,new Animation.MultiFrame(self.drawWheel,frames/10)
+                         ,new Animation.MultiFrame(self.tween(vert),frames/10)]);
+}
+
 
 /*
 This function disables the roll button in between turns
@@ -60,38 +162,3 @@ function drawRoll(rsum){
 
 
 
-/*__________________________________________
-Extra code: Can be deleted by end of project
-function diceScroll2(){
-  for (var i = 100; i >= 0; i--){
-    setInterval(rollDice(),100);
-  }
-}
-
-//Old diceRoll()
-function rollDice(){
-  var roll1 = Math.round(Math.random() * 5)+1;
-  var roll2 = Math.round(Math.random() * 5)+1;
-  var rsum = roll1 + roll2;
-  rollUpdate(rsum);
-  console.log(rsum);
-  // if (rsum == 7){ //will have to implement elsewhere when we start worrying about moving the robber
-  //  moveRobber();
-  // }
-}
-
-function diceScroll(){
-  var rollHolder = document.getElementById("rollValue");
-  for (var i = 10000; i >= 0; i--) {
-    setTimeout(function()
-    {
-      var roll1 = Math.round(Math.random() * 5)+1;
-      var roll2 = Math.round(Math.random() * 5)+1;
-      var rsum = roll1 + roll2;
-      rollHolder.innerHTML = rsum;
-    }, 100);
-  };
-}
-
-
-*/

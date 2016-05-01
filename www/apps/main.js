64398a88f59cce8f22e1ed8815ad28960860e5a2
Hunter Herman <hherman1@macalester.edu>
2016-04-30 19:41:58 -0500

    Remove commented code

--- www/apps/main.js
+++ www/apps/main.js
@@ -7,39 +7,6 @@ requirejs.config({
 });
 
 define(function (require) {
-    // Load any app-specific modules
-    // with a relative require call,
-    // like:
-
-
-        /*
-    var types = require("./Types");
-    var view = require('./View');
-    var game = require('./Game');
-    var gameM = require('./GameMethods');
-    var boardstate = require('./BoardState');
-    var constants = require('./Constants');
-    var renderTree = require('./RenderTree');
-    var canvas = require('./Canvas');
-    var animation = require('./Animation');
-    var grid = require('./Grid');
-    var hitbox = require('./Hitbox');
-    var mouse = require('./Mouse');
-    var player = require('./Player');
-    var robber = require('./Robber');
-    var structrender = require('./StructureRenderer');
-    var action = require('./Action');
-    var uiController = require('./UserInterfaceJScript');
-    var tradeOffers = require('./TradeOffer');
-    var tradeViews = require('./TradeView');
-    var longestroadview = require('./LongestRoadView');
-    var dicerollview = require('./DiceRollView');
-    var hexView = require('./HexInfoView');
-    var hexView = require('./PassView');
-    main();
-
-    */
     Init = require("./Init");
     Init.main();
-//    console.log(Grid.rotationMatrix(15)); 
 });

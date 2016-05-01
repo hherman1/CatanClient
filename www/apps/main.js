cf8bf2cbb454593bf8eef121119d0a0424cb6270
Hunter Herman <hherman1@macalester.edu>
2016-04-30 19:33:56 -0500

    Load gh-pages javascript

--- www/apps/main.js
+++ www/apps/main.js
@@ -1,6 +1,6 @@
 
 requirejs.config({
-    baseUrl: 'https://rawgit.com/hherman1/CatanClient/master/www/apps/',
+    baseUrl: 'https://rawgit.com/hherman1/CatanClient/gh-pages/www/apps/',
     paths: {
         jquery: "https://ajax.googleapis.com/ajax/libs/jquery/1.12.0/jquery.min",
     }
